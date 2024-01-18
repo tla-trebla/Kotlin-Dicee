f71c217ec0c2a27863d784f59c71deff4eeca6d7
Albert Pangestu <albert.p@fore.coffee>
2024-01-18 23:58:43 +0700

    Added background image using `Box` with its properties that has been separated into different variables

--- app/src/main/java/com/example/dicee/MainActivity.kt
+++ app/src/main/java/com/example/dicee/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -11,6 +12,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dicee.ui.theme.DiceeTheme
@@ -21,11 +24,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiceeTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    BackgroundBox()
                     MainColumn()
                 }
             }
         }
     }
+}
+
+@Composable
+fun BackgroundBox() {
+    val paintModifier = Modifier.paint(
+        painter = painterResource(id = R.drawable.background),
+        contentScale = ContentScale.FillBounds
+    )
+    val modifier = Modifier
+        .fillMaxSize()
+        .then(paintModifier)
+    Box(modifier = modifier)
 }
 
 @Composable
@@ -47,6 +63,7 @@ fun LogoImage() {
 @Composable
 fun GreetingPreview() {
     DiceeTheme {
+        BackgroundBox()
         MainColumn()
     }
 }
