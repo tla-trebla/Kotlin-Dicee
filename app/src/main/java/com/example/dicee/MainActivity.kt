b2ba5e19ad8fcc2aac6aed61ecfea5cfe843d9a8
Albert Pangestu <albert.p@fore.coffee>
2024-01-21 00:17:01 +0700

    Improved the UI layout

--- app/src/main/java/com/example/dicee/MainActivity.kt
+++ app/src/main/java/com/example/dicee/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.dicee
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +23,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
@@ -26,6 +31,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dicee.ui.theme.DiceeTheme
 import kotlin.random.Random
 
@@ -61,12 +67,17 @@ fun MainColumn() {
     var firstDice by remember { mutableIntStateOf(1) }
     var secondDice by remember { mutableIntStateOf(1) }
 
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
         LogoImage()
+        Spacer(modifier = Modifier.size(20.dp))
         Row {
             DiceImage(number = firstDice)
+            Spacer(modifier = Modifier.size(16.dp))
             DiceImage(number = secondDice)
         }
+        Spacer(modifier = Modifier.size(20.dp))
         RollButton(onClick = {
             firstDice = (1..6).random()
             secondDice = (1..6).random()
